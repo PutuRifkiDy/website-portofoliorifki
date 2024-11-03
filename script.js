95ef9a6932a32080ab76dc0c64a6f809a309be10
PutuRifkiDy <puturifki56@gmail.com>
2024-11-03 11:57:20 +0800

    add: rensponsive

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function validateEmail() {
         alert("Thank you for providing a valid email!");
     }
 }
+
+// fungsi untuk navbar saat tampilan mobile
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navbar = document.querySelectorAll(".nav");
+
+    hamburger.addEventListener("click", function() {
+        navbar.forEach(nav => {
+            nav.classList.toggle("active");
+        });
+    });
+});
